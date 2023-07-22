fe9fde3bd4ce88a152e515f97e8895ca7b51483b
jehielmartinez <jehiel.martinez@codexitos.com>
2023-07-22 12:07:01 -0600

    Initial commit

---
+++ test/highly-available-ec2-rds-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HighlyAvailableEc2RdsService from '../lib/highly-available-ec2-rds-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/highly-available-ec2-rds-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HighlyAvailableEc2RdsService.HighlyAvailableEc2RdsServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
